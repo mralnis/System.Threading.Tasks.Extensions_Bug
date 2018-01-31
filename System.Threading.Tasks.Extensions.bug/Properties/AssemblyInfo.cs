f61d0673b60d7a2f87ae4881c66221d54d6d23dc
Stivriņš, Didzis <Didzis.Stivrins@if.lv>
2018-01-31 15:35:45 +0200

    Add project files.

---
+++ System.Threading.Tasks.Extensions.bug/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("System.Threading.Tasks.Extensions.bug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("System.Threading.Tasks.Extensions.bug")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5c3ccd3f-4a92-4b8d-ad83-b677c7fe57a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ System.Threading.Tasks.Extensions.bug/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Rest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+
+namespace System.Threading.Tasks.Extensions.bug
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MethodNotFound()
+        {
+            var _result = new HttpOperationResponse<object>();
+            _result.Request = new HttpRequestMessage();
+        }
+    }
+}
